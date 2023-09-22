bbae9c5afb21d979e1833caf577a7f53cde0f626
nitheshracharla <nithesh.r@praxis.ac.in>
2023-09-22 11:50:42 +0530

    Demo change

--- 02 - Styling and Templates/market.py
+++ 02 - Styling and Templates/market.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+#  demo
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+@app.route('/demo')
+def demo():
+    return "Demo!"
